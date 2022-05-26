866676314a5598bbd8aef4ea28269c703fc1888d
David Wang <pro.d.wang@gmail.com>
2022-05-25 18:40:20 -0700

    feat: content

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          How about now?
+          How about now? Step 2
         </p>
         <a
           className="App-link"
