2e8083d809bf528e2abddda7c5222dd0ed441693
David Wang <pro.d.wang@gmail.com>
2022-05-25 19:25:13 -0700

    feat: step 4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is great step 3
+          This is great step 4
         </p>
       </header>
     </div>
